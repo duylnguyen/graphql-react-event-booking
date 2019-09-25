a00d47fd64c81acc69e58e0fe7c8c15ebb7d7e5f
Duy Nguyen <duy_nobita@icloud.com>
2019-09-25 17:06:28 -0400

    Adding Booking model and cancelBooking

--- models/event.js
+++ models/event.js
@@ -22,7 +22,8 @@ const eventSchema = new Schema({
     creator: {
         type: Schema.Types.ObjectId,
         ref: 'User'
-    }
+    },
+    
 })
 
 module.exports = mongoose.model('Event', eventSchema)
